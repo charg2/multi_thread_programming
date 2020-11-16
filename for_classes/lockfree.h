745269e41e4eadd932fd4058e458f40000b98e9b
charg2dang <55304275+charg2dang@users.noreply.github.com>
2020-11-17 05:35:12 +0900

    Update lockfree.h

--- for_classes/lockfree.h
+++ for_classes/lockfree.h
@@ -3,13 +3,34 @@
 
 using namespace std;
 
-bool cas(atomic_int* addr, int expected, int new_value)
+bool cas(atomic<size_t>* addr, size_t expected, size_t new_value)
 {
 	return atomic_compare_exchange_strong(addr, &expected, new_value);
 }
 
-bool cas(volatile int* addr, int expected, int new_val) 
+bool cas(volatile size_t* addr, size_t expected, size_t new_val) 
 { 
-	return atomic_compare_exchange_strong(reinterpret_cast<volatile atomic_int*>(addr), &expected, new_val); 
+	return atomic_compare_exchange_strong(reinterpret_cast<volatile atomic<size_t>*>(addr), &expected, new_val); 
+}
+
+
+bool cas32(atomic<int>* addr, int expected, int new_value)
+{
+	return atomic_compare_exchange_strong(addr, &expected, new_value);
+}
+
+bool cas32(volatile int* addr, int expected, int new_val)
+{
+	return atomic_compare_exchange_strong(reinterpret_cast<volatile atomic<int>*>(addr), &expected, new_val);
+}
+
+bool cas64(atomic<long long>* addr, long long expected, long long new_value)
+{
+	return atomic_compare_exchange_strong(addr, &expected, new_value);
+}
+
+bool cas64(volatile long long* addr, long long expected, long long new_val)
+{
+	return atomic_compare_exchange_strong(reinterpret_cast<volatile atomic<long long>*>(addr), &expected, new_val);
 }
 
