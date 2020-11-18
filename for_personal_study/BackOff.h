37b12f89c1901de88783e4c8227317d45611468b
charg2dang <55304275+charg2dang@users.noreply.github.com>
2020-11-18 17:43:53 +0900

    Update BackOff.h

--- for_personal_study/BackOff.h
+++ for_personal_study/BackOff.h
@@ -18,3 +18,17 @@ public:
 	static inline constexpr size_t max_delay{ 1000 };
 };
 
+
+struct BackOffSleep
+{
+public:
+	void do_backoff_sleep();
+
+public:
+	int limit;
+
+	static inline constexpr size_t min_delay{ 1 };
+	static inline constexpr size_t max_delay{ 10 };
+};
+
+
