fb55e27e487b8695df9b1e31c6797b7ffe792e83
charg2dang <55304275+charg2dang@users.noreply.github.com>
2020-12-03 23:58:30 +0900

    Update lazy_skip_list.h

--- for_classes/lazy_skip_list.h
+++ for_classes/lazy_skip_list.h
@@ -6,11 +6,10 @@
 #include <iostream>
 
 
-#include "fast_rand.hpp"
+//#include "fast_rand.hpp" #define RAND() fast_rand()
 #include <random>
 
-#define RAND() fast_rand()
-//#define RAND() rand()
+#define RAND() rand()
 
 
 
@@ -29,15 +28,15 @@ class lazy_skip_list
 		node_t*					next[MAX_LEVEL];
 		int						key;
 		int						height;
-		volatile bool			removed;
-		volatile bool			fully_linked;
-		std::recursive_mutex	mtx;
+		volatile bool			is_removed;
+		volatile bool			is_fully_linked;
+		recursive_mutex			mtx;
 
-		node_t() : next{}, key{}, height{ MAX_LEVEL }, removed{ }, fully_linked{}
+		node_t() : next{}, key{}, height{ MAX_LEVEL }, is_removed{ }, is_fully_linked{}
 		{}
-		node_t(int my_key) : next{}, key{ my_key }, height{ MAX_LEVEL }, removed{ }, fully_linked{}
+		node_t(int my_key) : next{}, key{ my_key }, height{ MAX_LEVEL }, is_removed{ }, is_fully_linked{}
 		{}
-		node_t(int my_key, int height) : next{}, key{ my_key }, height{ height }, removed{}, fully_linked{}
+		node_t(int my_key, int height) : next{}, key{ my_key }, height{ height }, is_removed{}, is_fully_linked{}
 		{}
 
 		void lock()
@@ -59,8 +58,9 @@ private:
 public:
 	lazy_skip_list() : head{ 0x80000000L }, tail{ 0x7FFFFFFFL }
 	{
+		head.is_fully_linked = true;
+		tail.is_fully_linked = true;
 		//head.level = tail.level = MAX_LEVEL;
-
 
 		for (auto& ptr : head.next)
 		{
@@ -89,14 +89,14 @@ public:
 		}
 	}
 
-	int find(int key, node_t* preds[MAX_LEVEL], node_t* currs[MAX_LEVEL])
+	int find(int key, node_t* preds[], node_t* currs[])
 	{
-		int		current_level	= MAX_LEVEL - 1;
-		int		founded			= -1;
-		node_t* pred			= &head;
+		int		current_level = MAX_LEVEL - 1;
+		int		founded = -1;
+		node_t* pred = &head;
 
 
-		for ( int level = MAX_LEVEL-1; level >= 0; level -=1 )
+		for (int level = MAX_LEVEL - 1; level >= 0; level -= 1)
 		{
 			volatile node_t* curr = pred->next[level];
 			while (key > curr->key)
@@ -105,6 +105,7 @@ public:
 				curr = pred->next[level];
 			}
 
+			// 최초 찾앗는지 founded가 -1이면 
 			if (founded == -1 && key == curr->key)
 			{
 				founded = level;
@@ -114,6 +115,8 @@ public:
 			currs[level] = curr;
 		}
 
+
+		// 최초 찾은 레벨.
 		return founded;
 	}
 
@@ -146,21 +149,23 @@ public:
 		while (true)
 		{
 			int founded = find(key, preds, succs);
-			if (-1 != founded)
+			if (-1 != founded) // 찾았으면.. 
 			{
 				node_t* node_found = succs[founded];
-				if (false == node_found->removed)
+				if (false == node_found->is_removed) // 찾았는데 마킹이 되어 
 				{
-					while (false == node_found->fully_linked)
+					while (false == node_found->fully_linked) // 마킹 기다림...
 					{}
 
-					return false;
+					return false; // 이미 있는 경우.
 				}
-
-				continue;
+				else // invliad 
+				{
+					continue;
+				}
 			}
 
-			int highest_locked = -1;
+			int highest_locked = -1; // 어디까지 락이 되어 있는가.
 			{
 				node_t* pred;
 				node_t* succ;
@@ -173,25 +178,26 @@ public:
 					succ = succs[level];
 					pred->lock();
 					highest_locked = level;
-					valid = !pred->removed && !succ->removed && pred->next[level] == succ;
+					valid = !pred->is_removed && !succ->is_removed && pred->next[level] == succ;
 				}
 
-				if ( false == valid )
-				{
-					continue;
-				}
+				//if (false == valid)
+				//{
+				//	continue;
+				//}
 
 
+				// 유효하지 안다면 바져 나감.
 				if (false == valid)
 				{
 					for (int level = 0; level <= highest_locked; ++level)
 					{
-						preds[level]->unlock();
+						preds[level]->unlock(); // 락 건수만큼 unlock을 해줌.
 					}
 					continue;
 				}
 
-				node_t* new_node = new node_t{key, top_level};
+				node_t* new_node = new node_t{ key, top_level };
 				for (int level = 0; level <= top_level; ++level)
 				{
 					new_node->next[level] = succs[level];
@@ -210,10 +216,11 @@ public:
 				{
 					preds[level]->unlock();
 				}
+
 				return true;
 			}
 		}
-	
+
 	}
 
 	bool remove(int key)
@@ -229,18 +236,18 @@ public:
 			int founded = find(key, preds, succs);
 			if (founded != -1) victim = succs[founded];
 			if (is_removed
-				|| (founded != -1 && (victim->fullyLinked && victim->top_level == founded && !victim->removed)))
+				|| (founded != -1 && (victim->fullyLinked && victim->top_level == founded && !victim->is_removed)))
 			{
 				if (!is_removed)
 				{
 					top_level = victim->top_level;
 					victim->lock();
-					if (victim->removed)
+					if (victim->is_removed)
 					{
 						victim->unlock();
 						return false;
 					}
-					victim->removed = true;
+					victim->is_removed = true;
 					is_removed = true;
 				}
 				int highest_locked = -1;
@@ -253,7 +260,7 @@ public:
 						pred = preds[level];
 						pred->lock();
 						highest_locked = level;
-						valid = !pred->removed && pred->next[level] == victim;
+						valid = !pred->is_removed && pred->next[level] == victim;
 					}
 					if (!valid)
 					{
@@ -296,7 +303,7 @@ public:
 		{
 			cout << p->key << ", ";
 
-			p = p->next[0]; 
+			p = p->next[0];
 			c--;
 
 			if (c == 0)
